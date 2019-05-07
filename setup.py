a185cbb046669a5bec7af0ed69677a86a8f1fcf0
Nezar <nabdennur@gmail.com>
2019-05-07 12:22:34 -0400

    Change script to use module entrypoint and console-scripts

--- setup.py
+++ setup.py
@@ -6,8 +6,12 @@ setup(
     name='simple-httpfs',
     author='Peter Kerpedjiev',
     author_email='pkerpedjiev@gmail.com',
-    packages=[ 'simple_httpfs' ],
-    scripts=['simple_httpfs/simple-httpfs.py'],
+    packages=['simple_httpfs'],
+    entry_points={
+      'console_scripts': [
+          'httpfs = simple_httpfs.__main__:main'
+      ]
+    },
     url='https://github.com/higlass/simple-httpfs',
     license='LICENSE.txt',
     description='A simple FUSE filesystem for reading http files',
