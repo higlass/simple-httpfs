1579a1d2abb4564c0c7fb2f4a2dee7eaf42cc1cb
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2020-03-03 22:14:29 -0800

    Working first version of an ftp fetcher

--- simple_httpfs/httpfs.py
+++ simple_httpfs/httpfs.py
@@ -15,16 +15,18 @@ from time import time
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 import diskcache as dc
 
+from ftplib import FTP
+from urllib.parse import urlparse
 
 CLEANUP_INTERVAL = 60
 CLEANUP_EXPIRED = 60
 
 
-DISK_CACHE_SIZE_ENV = 'HTTPFS_DISK_CACHE_SIZE'
-DISK_CACHE_DIR_ENV = 'HTTPFS_DISK_CACHE_DIR'
+DISK_CACHE_SIZE_ENV = "HTTPFS_DISK_CACHE_SIZE"
+DISK_CACHE_DIR_ENV = "HTTPFS_DISK_CACHE_DIR"
 
 
-FALSY = {0, '0', False, 'false', 'False', 'FALSE', 'off', 'OFF'}
+FALSY = {0, "0", False, "false", "False", "FALSE", "off", "OFF"}
 
 
 class LRUCache:
@@ -52,24 +54,116 @@ class LRUCache:
         return len(self.cache)
 
 
+class FtpFetcher:
+    def server_path(self, url):
+        o = urlparse(url)
+
+        return (o.netloc, o.path)
+
+    def login(self, server):
+        ftp = FTP(server)
+        ftp.login()
+
+        return ftp
+
+    def get_size(self, url):
+        (server, path) = self.server_path(url)
+
+        ftp = self.login(server)
+        size = ftp.size(path)
+        ftp.close()
+        return size
+
+    def get_data(self, url, start, end):
+        import time
+
+        (server, path) = self.server_path(url)
+        t1 = time.time()
+        print("getting: {}-{}", start, end)
+
+        ftp = self.login(server)
+        conn = ftp.transfercmd("RETR {}".format(path), rest=start)
+
+        amt = end - start
+        chunk_size = 1 << 15
+        data = []
+        while len(data) < amt:
+            chunk = conn.recv(chunk_size)
+            if chunk:
+                data += chunk
+            else:
+                break
+        if len(data) < amt:
+            data += [0] * (amt - len(data))
+        else:
+            data = data[:amt]
+
+        ftp.close()
+        t2 = time.time()
+        print(t2 - t1)
+        return np.array(data, dtype=np.uint8)
+
+
+class HttpFetcher:
+    def get_size(self, url):
+        try:
+            head = requests.head(url, allow_redirects=True, verify=self.SSL_VERIFY)
+            return int(head.headers["Content-Length"])
+        except:
+            head = requests.get(
+                url,
+                allow_redirects=True,
+                verify=self.SSL_VERIFY,
+                headers={"Range": "bytes=0-1"},
+            )
+            crange = head.headers["Content-Range"]
+            match = re.search(r"/(\d+)$", crange)
+            if match:
+                return int(match.group(1))
+
+            logging.error(traceback.format_exc())
+            raise FuseOSError(ENOENT)
+
+    def get_data(self, url, start, end):
+        headers = {"Range": "bytes={}-{}".format(start, end), "Accept-Encoding": ""}
+        r = requests.get(url, headers=headers)
+        block_data = np.frombuffer(r.content, dtype=np.uint8)
+        return block_data
+
+
 class HttpFs(LoggingMixIn, Operations):
     """
     A read only http/https/ftp filesystem.
 
     """
-    SSL_VERIFY = os.environ.get('SSL_VERIFY', True) not in FALSY
 
-    def __init__(self, schema, disk_cache_size=2**30,
-                 disk_cache_dir='/tmp/xx', lru_capacity=400, block_size=2**20):
+    SSL_VERIFY = os.environ.get("SSL_VERIFY", True) not in FALSY
+
+    def __init__(
+        self,
+        schema,
+        disk_cache_size=2 ** 30,
+        disk_cache_dir="/tmp/xx",
+        lru_capacity=400,
+        block_size=2 ** 20,
+    ):
         if not self.SSL_VERIFY:
             logging.warning(
-                'You have set ssl certificates to not be verified. '
-                'This may leave you vulnerable. '
-                'http://docs.python-requests.org/en/master/user/advanced/#ssl-cert-verification')
+                "You have set ssl certificates to not be verified. "
+                "This may leave you vulnerable. "
+                "http://docs.python-requests.org/en/master/user/advanced/#ssl-cert-verification"
+            )
 
         self.lru_cache = LRUCache(capacity=lru_capacity)
         self.lru_attrs = LRUCache(capacity=lru_capacity)
         self.schema = schema
+
+        if schema == "http" or schema == "https":
+            self.fetcher = HttpFetcher()
+        elif schema == "ftp":
+            self.fetcher = FtpFetcher()
+        else:
+            raise ("Unknown schema: {}".format(schema))
 
         self.disk_cache = dc.Cache(disk_cache_dir, disk_cache_size)
 
@@ -81,38 +175,22 @@ class HttpFs(LoggingMixIn, Operations):
         self.block_size = block_size
 
     def getSize(self, url):
-        try:
-            head = requests.head(url, allow_redirects=True,
-                                 verify=self.SSL_VERIFY)
-            return int(head.headers['Content-Length'])
-        except:
-            head = requests.get(
-                url,
-                allow_redirects=True,
-                verify=self.SSL_VERIFY,
-                headers={'Range': 'bytes=0-1'})
-            crange = head.headers['Content-Range']
-            match = re.search(r'/(\d+)$', crange)
-            if match:
-                return int(match.group(1))
-
-            logging.error(traceback.format_exc())
-            raise FuseOSError(ENOENT)
+        return self.fetcher.get_size(url)
 
     def getattr(self, path, fh=None):
-        #logging.info("attr path: {}".format(path))
+        # logging.info("attr path: {}".format(path))
 
         if path in self.lru_attrs:
             return self.lru_attrs[path]
 
-        if path == '/':
+        if path == "/":
             self.lru_attrs[path] = dict(st_mode=(S_IFDIR | 0o555), st_nlink=2)
             return self.lru_attrs[path]
 
-        if path[-2:] != '..':
+        if path[-2:] != "..":
             return dict(st_mode=(S_IFDIR | 0o555), st_nlink=2)
 
-        url = '{}:/{}'.format(self.schema, path[:-2])
+        url = "{}:/{}".format(self.schema, path[:-2])
 
         # logging.info("attr url: {}".format(url))
         size = self.getSize(url)
@@ -128,19 +206,24 @@ class HttpFs(LoggingMixIn, Operations):
                 st_size=size,
                 st_ctime=time(),
                 st_mtime=time(),
-                st_atime=time())
+                st_atime=time(),
+            )
         else:
             self.lru_attrs[path] = dict(st_mode=(S_IFDIR | 0o555), st_nlink=2)
 
         return self.lru_attrs[path]
 
     def read(self, path, size, offset, fh):
-        #logging.info("read path: {}".format(path))
+        # logging.info("read path: {}".format(path))
         if path in self.lru_attrs:
-            url = '{}:/{}'.format(self.schema, path[:-2])
+            url = "{}:/{}".format(self.schema, path[:-2])
 
             logging.info("read url: {}".format(url))
-            logging.info("offset: {} - {} request_size (KB): {:.2f} block: {}".format(offset, offset + size - 1, size / 2 ** 10, offset // self.block_size))
+            logging.info(
+                "offset: {} - {} request_size (KB): {:.2f} block: {}".format(
+                    offset, offset + size - 1, size / 2 ** 10, offset // self.block_size
+                )
+            )
             output = np.zeros((size,), np.uint8)
 
             t1 = time()
@@ -156,19 +239,17 @@ class HttpFs(LoggingMixIn, Operations):
                 block_data = self.get_block(url, block_num)
 
                 data_start = (
-                    curr_start -
-                    (curr_start // self.block_size) * self.block_size
+                    curr_start - (curr_start // self.block_size) * self.block_size
                 )
 
                 data_end = min(self.block_size, offset + size - block_start)
                 data = block_data[data_start:data_end]
 
                 d_start = curr_start - offset
-                output[d_start:d_start+len(data)] = data
-
+                output[d_start : d_start + len(data)] = data
 
                 last_fetched = curr_start + (data_end - data_start)
-                curr_start += (data_end - data_start)
+                curr_start += data_end - data_start
 
             t2 = time()
 
@@ -176,9 +257,10 @@ class HttpFs(LoggingMixIn, Operations):
             # logging.info(url)
             # logging.info(headers)
             logging.info(
-                "lru hits: {} lru misses: {} disk hits: {} disk misses: {}"
-                .format(self.lru_hits, self.lru_misses, self.disk_hits,
-                        self.disk_misses))
+                "lru hits: {} lru misses: {} disk hits: {} disk misses: {}".format(
+                    self.lru_hits, self.lru_misses, self.disk_hits, self.disk_misses
+                )
+            )
 
             logging.info("time: {:.4f}".format(t2 - t1))
             return bytes(output)
@@ -191,7 +273,7 @@ class HttpFs(LoggingMixIn, Operations):
         self.disk_cache.close()
 
     def get_block(self, url, block_num):
-        '''
+        """
         Get a data block from a URL. Blocks are 256K bytes in size
 
         Parameters:
@@ -200,8 +282,8 @@ class HttpFs(LoggingMixIn, Operations):
             The url of the file we want to retrieve a block from
         block_num: int
             The # of the 256K'th block of this file
-        '''
-        cache_key=  "{}.{}.{}".format(url, self.block_size, block_num)
+        """
+        cache_key = "{}.{}.{}".format(url, self.block_size, block_num)
         cache = self.disk_cache
 
         if cache_key in self.lru_cache:
@@ -223,14 +305,10 @@ class HttpFs(LoggingMixIn, Operations):
             self.disk_misses += 1
             block_start = block_num * self.block_size
 
-            headers = {
-                'Range': 'bytes={}-{}'.format(block_start,
-                    block_start + self.block_size - 1),
-                'Accept-Encoding': ''
-            }
-            r = requests.get(url, headers=headers)
-            content = r.content
-            block_data = np.frombuffer(r.content, dtype=np.uint8)
+            block_data = self.fetcher.get_data(
+                url, block_start, block_start + self.block_size - 1
+            )
+
             self.lru_cache[cache_key] = block_data
             self.disk_cache[cache_key] = block_data
 
