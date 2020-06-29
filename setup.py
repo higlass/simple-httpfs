07c20bed8e8d94c7ac044afd82dfb066d755bf78
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2020-06-28 20:33:13 -0700

    Added versions and setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
-import versioneer
 
 setup(
-    name='simple-httpfs',
-    author='Peter Kerpedjiev',
-    author_email='pkerpedjiev@gmail.com',
-    packages=['simple_httpfs'],
-    entry_points={
-      'console_scripts': [
-          'simple-httpfs = simple_httpfs.__main__:main'
-      ]
-    },
-    url='https://github.com/higlass/simple-httpfs',
-    description='A simple FUSE filesystem for reading http files',
-    license='MIT',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    install_requires=[
-        "boto3",
-        "diskcache",
-        "fusepy",
-        "requests",
-        "slugid",
-    ],
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass()
+    name="simple-httpfs",
+    author="Peter Kerpedjiev",
+    author_email="pkerpedjiev@gmail.com",
+    packages=["simple_httpfs"],
+    entry_points={"console_scripts": ["simple-httpfs = simple_httpfs.__main__:main"]},
+    url="https://github.com/higlass/simple-httpfs",
+    description="A simple FUSE filesystem for reading http files",
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["boto3", "diskcache", "fusepy", "requests", "slugid",],
+    version="0.4.0",
 )
